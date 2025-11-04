ce0617f93b5baa1850ec3ee8ddc07841f1e7852a
anonymous <anonymous@example.com>
2025-11-03 21:58:20 -0300

    Update alias_repository_impl.dart

--- lib/data/repositories/alias_repository_impl.dart
+++ lib/data/repositories/alias_repository_impl.dart
@@ -1,11 +1,13 @@
+import 'package:injectable/injectable.dart';
+
 import '../../domain/entities/alias_entity.dart';
 import '../../domain/repositories/alias_repository.dart';
 import '../datasources/url_shortener_api.dart';
 
+@LazySingleton(as: AliasRepository)
 class AliasRepositoryImpl implements AliasRepository {
   final UrlShortenerApi api;
-
-  AliasRepositoryImpl(this.api);
+  const AliasRepositoryImpl(this.api);
 
   @override
   Future<AliasEntity> shortenUrl(String url) async {
