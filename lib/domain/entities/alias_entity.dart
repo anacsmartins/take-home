ed84c11a96a59f3ebdb20940940e683386e366e5
anonymous <anonymous@example.com>
2025-11-03 23:38:20 -0300

    Create alias_entity.dart

---
+++ lib/domain/entities/alias_entity.dart
@@ -0,0 +1,11 @@
+class AliasEntity {
+  final String alias;
+  final String originalUrl;
+  final String shortUrl;
+
+  const AliasEntity({
+    required this.alias,
+    required this.originalUrl,
+    required this.shortUrl,
+  });
+}
