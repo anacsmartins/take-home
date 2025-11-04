f410c0f68ec2593b74479af51fdeb190a0a48cd5
anonymous <anonymous@example.com>
2025-11-03 21:22:18 -0300

    Implement ShortenerCubit for URL shortening

---
+++ lib/presentation/cubit/shortener_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../domain/usecases/shorten_url_usecase.dart';
+import 'shortener_state.dart';
+
+class ShortenerCubit extends Cubit<ShortenerState> {
+  final ShortenUrlUseCase usecase;
+
+  ShortenerCubit(this.usecase) : super(ShortenerInitial());
+
+  Future<void> shorten(String url) async {
+    emit(ShortenerLoading());
+    try {
+      final entity = await usecase(url);
+
+      final current = state;
+      if (current is ShortenerSuccess) {
+        final updated = [...current.list, entity];
+        emit(ShortenerSuccess(updated));
+      } else {
+        emit(ShortenerSuccess([entity]));
+      }
+    } catch (e) {
+      emit(ShortenerError(e.toString()));
+    }
+  }
+}
