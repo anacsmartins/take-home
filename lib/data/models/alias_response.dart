7864249a4f52f185b69bbcc0a12610d6d4a6ae06
anonymous <anonymous@example.com>
2025-11-03 21:19:57 -0300

    Add AliasResponse model with JSON parsing

---
+++ lib/data/models/alias_response.dart
@@ -0,0 +1,28 @@
+import '../../domain/entities/alias_entity.dart';
+
+class AliasResponse {
+  final String alias;
+  final String originalUrl;
+  final String shortUrl;
+
+  const AliasResponse({
+    required this.alias,
+    required this.originalUrl,
+    required this.shortUrl,
+  });
+
+  factory AliasResponse.fromJson(Map<String, dynamic> json) {
+    final links = json['_links'] as Map<String, dynamic>? ?? const {};
+    return AliasResponse(
+      alias: json['alias'] as String,
+      originalUrl: (links['self'] ?? '') as String,
+      shortUrl: (links['short'] ?? '') as String,
+    );
+  }
+
+  AliasEntity toEntity() => AliasEntity(
+        alias: alias,
+        originalUrl: originalUrl,
+        shortUrl: shortUrl,
+      );
+}
